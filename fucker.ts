7b01a726234018e59ee9bb4f23c79f34d1ffabb2
rare1k <hi@rare1k.dev>
2024-05-08 19:06:39 -0400

    feat: start working on menu

--- fucker.ts
+++ fucker.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+const colors = require('colors');
+console.clear();
+
+console.log("Surfskip Fucker 2.0".rainbow + " | ".gray + "by: rare1k\n".blue.bold);
+
+console.log("🧑‍🌾" + " Scraping Method: ".blue)
+console.log("| ".gray + "🔗" + " [1]".brightBlue.bold + " links.surfskip.com")
+console.log("| ".gray + "📄" + " [2]".brightBlue.bold + " Google Docs")
+
+console.log()
