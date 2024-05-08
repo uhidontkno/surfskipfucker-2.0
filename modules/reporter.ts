56afd4e91f55001bc5acca7fe6559bfc4a184550
rare1k <hi@rare1k.dev>
2024-05-08 17:22:19 -0400

    feat: add lightspeed reporting

---
+++ modules/reporter.ts
@@ -0,0 +1,14 @@
+async function reportLightspeed(email,domain,reason) {
+    let res = await fetch("https://production-archive-proxy-api.lightspeedsystems.com/archiveproxy", {
+        "headers": {
+          "accept": "application/json, text/plain, */*",
+          "accept-language": "en-US,en;q=0.5",
+          "content-type": "application/json",
+          "x-api-key": "onEkoztnFpTi3VG7XQEq6skQWN3aFm3h"
+        },
+        "referrerPolicy": "same-origin",
+        "body": `{\"query\":\"query SubmitIntoBFR($item: BFRSubmission!){\\n        item: custom_BFRSubmission(item: $item) {\\n            SubmittedURL\\n            ok\\n        }\\n      }\",\"variables\":{\"item\":{\"EmailAddress\":\"${email}\",\"Reason\":\"${reason}\",\"SubmittedURL\":\"${domain}\",\"Type\":\"DomainStandard\",\"Source\":\"archive\"}}}`,
+        "method": "POST"
+      });
+}
+export default {reportLightspeed}
