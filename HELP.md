124fbec7ffc42d7da417beb54e4caa1fd009f00b
rare1k <hi@rare1k.dev>
2024-05-25 13:30:16 -0400

    feat: tweaks

--- HELP.md
+++ HELP.md
@@ -1,7 +1,8 @@
-# surfskipfucker 2.0
-Why is surfskip trying to revive themselves? 
 ## Command Line Help:
 **`--help`** lists this
 **`--method [num]`** auto-picks the method of scraping for you
 **`--email [email]`** auto-fills-in the provided email, you can also provide `anonymous`, `none`, or `default` to use the default (anonymous) email
 **`--reason "[reason]"`** auto-fills-in the report reason, you can also provide `none` or `default` to use the default.
+
+*Github: [github.com/uhidontkno/surfskipfucker-2.0](https://github.com/uhidontkno/surfskipfucker-2.0)*
+*Licensed under the MIT license*
--- modules/help.ts
+++ modules/help.ts
@@ -1,11 +1,29 @@
 import markdownit from 'markdown-it'
 const md = markdownit()
-var terminal = require('markdown-it-terminal');
+const terminal = require('markdown-it-terminal');
 import styles from 'ansi-styles';
+
+function compoundStyle(styleList:any[]) {
+    var open  = '';
+    var close = '';
+    styleList.forEach(function(style:any){
+      open += styles[style].open;
+    });
+    styleList.reverse().forEach(function(style:any) {
+      close += styles[style].close;
+    });
+    
+    return {
+      open: open, 
+      close: close
+    };
+  };
+
 var options = {
     styleOptions: {
       code: styles.blue,
       codespan: styles.blue,
+      link: compoundStyle(["blue","underline"])
     }
   }
 md.use(terminal,options);
